05318b43fb14e59c87750003b4f106012b7e9544
ben bowman <ben.bowman@westnet.com.au>
2022-05-20 12:50:02 +1000

    creating visual representation of time domain

--- radarSystem.py
+++ radarSystem.py
@@ -1,6 +1,5 @@
 import numpy as np
 from matplotlib import pyplot as plt
-import math
 
 class chirp:
     def __init__(self, slope, bandwidth, f0):
@@ -10,16 +9,21 @@ class chirp:
         self.duration = self.bandwidth/slope
 
     def _signal_value(self, t):
-        return math.cos(self.slope*np.square(t)+self.f0*t)
+        return np.cos(self.slope*np.square(t)+self.f0*t)
 
 
     def signal_t(self, t, t0):
         t_shifted = t-t0
         signal = np.zeros_like(t)
-        signal[(t_shifted>0&t_shifted<self.duration)] = self._signal_value(t[(t_shifted>0&t_shifted<self.duration)])
+        nonzero = (t_shifted>0)&(t_shifted<self.duration)
+        signal[nonzero] = self._signal_value(t[nonzero])
 
         return signal
 
 if __name__ == "__main__":
     thing = chirp(slope = 1, bandwidth=10, f0=10)
-
+    t = np.linspace(0,20,2000)
+    t0 = 5
+    plt.plot(t,thing.signal_t(t,t0))
+    plt.show()
+    
